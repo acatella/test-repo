a7d01b138bb73d042934f1888b173cbfc7b61b3f
acatella <aficclark@gmail.com>
2016-04-09 12:23:27 -0700

    added click event that updates theme

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,13 @@
 console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
+
+$(document).ready(function() {
+
+  //Updates color theme on click
+  var themes = ['hero-blue','hero-pink'];
+  var $changeButton = $('div.hero');
+  $changeButton.on('click', function(e) {
+    var newTheme = themes[Math.floor((Math.random()*themes.length))];
+    console.log(newTheme);
+    $changeButton.attr('class', 'hero '+ newTheme);
+  });
+});
